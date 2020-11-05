1e80bd9ccaafcb53222e029bf9833ea5f7139689
shiba <shiba@alibaba-inc.com>
2020-11-05 10:36:42 +0800

    bugfix util to #31200783

--- superagent/index.js
+++ superagent/index.js
@@ -3,173 +3,112 @@ const config = require('../config/index');
 const cheerio = require('cheerio');
 const { machineIdSync } = require('node-machine-id');
 const crypto = require('crypto');
-let md5 = crypto.createHash('md5');
-let uniqueId = md5.update(machineIdSync()).digest('hex'); // 获取机器唯一识别码并MD5，方便机器人上下文关联
-const ONE = 'http://wufazhuce.com/'; // ONE的web版网站
-const TXHOST = 'http://api.tianapi.com/txapi/'; // 天行host
-const TULINGAPI = 'http://www.tuling123.com/openapi/api'; // 图灵1.0接口api
 
-async function getOne() {
+
+
+async function getONE(word){
+  word = word.trim();
+  const func = [history,rili,jiemeng, getQingYuke];
+  for(let i =0;i<func.length;i++){
+    let result = await func[i].call(this, word);
+    let hasRes = !(result === false || result === undefined);
+    if(hasRes){
+      return p1(result);
+    }
+  }
+  return '不知道呢';
+}
+
+// 兜底
+async function getQingYuke(word) {
   // 获取每日一句
   try {
-    let res = await superagent.req(ONE, 'GET');
-    let $ = cheerio.load(res.text);
-    let todayOneList = $('#carousel-one .carousel-inner .item');
-    let todayOne = $(todayOneList[0])
-      .find('.fp-one-cita')
-      .text()
-      .replace(/(^\s*)|(\s*$)/g, '');
-    return todayOne;
+    let res = JSON.parse(
+      (
+        await superagent.req(
+          'http://api.qingyunke.com/api.php?key=free&appid=0&msg=' +
+            encodeURIComponent(word),
+          'GET'
+        )
+      ).text
+    );
+    return res ? res.content : '不知道呢';
   } catch (err) {
     console.log('错误', err);
-    return err;
+    return false;
   }
 }
 
-async function getTXweather() {
-  // 获取天行天气
-  let url = TXHOST + 'tianqi/';
-  try {
-    let res = await superagent.req(url, 'GET', {
-      key: config.TXAPIKEY,
-      city: config.CITY
-    });
-    let content = JSON.parse(res.text);
-    if (content.code === 200) {
-      let todayInfo = content.newslist[0];
-      let obj = {
-        weatherTips: todayInfo.tips,
-        todayWeather:`今天${todayInfo.weather}<br>温度:${todayInfo.lowest}/${todayInfo.highest}<br>${todayInfo.wind} ${todayInfo.windspeed}<br>空气:${todayInfo.air_level} ${todayInfo.air}<br>`
-      };
-      console.log('获取天行天气成功', obj);
-      return obj;
-    } else {
-      console.log('获取接口失败', content.code);
+function p1(result) {
+  return result
+    .replace(/菲菲姐/g, '刘助理')
+    .replace(/菲菲/g, '刘助理')
+    .replace(/\{br\}/g, '\n');
+}
+
+//老黄历
+async function rili(word) {
+  if (word.includes('老黄历') || word.includes('黄历')) {
+    try {
+      const date = new Date();
+      let dstr = date.getFullYear() + '-' + (date.getMonth() + 1) + "-" + date.getDate();
+ 
+      let res = await superagent.req(
+        `http://v.juhe.cn/laohuangli/d?date=${dstr}&key=7f31c864b61c272b5e90adf7ca0a3568`
+      );
+      let obj = JSON.parse(res.text).result;
+      return Object.keys(obj)
+        .map((k) => k + ':' + obj[k] + '\n')
+        .join();
+    } catch {
+      return false;
     }
-  } catch (err) {
-    console.log('获取接口失败', err);
   }
+  return false;
 }
 
-// 天行对接的图灵机器人
-async function getTXTLReply(word) {
-  let url = TXHOST + 'tuling/';
-  let res = await superagent.req(url, 'GET', {
-    key: config.TXAPIKEY,
-    question: word,
-    userid: uniqueId
-  });
-  let content = JSON.parse(res.text);
-  if (content.code === 200) {
-    console.log('天行对接的图灵机器人:', content);
-    let response = content.newslist[0].reply;
-    return response;
-  } else {
-    return '我好像迷失在无边的网络中了，接口调用错误：' + content.msg;
-  }
-}
-
-// 图灵智能聊天机器人
-async function getTuLingReply(word) {
-  let url = TULINGAPI;
-  let res = await superagent.req(url, 'GET', {
-    key: config.TULINGKEY,
-    info: word
-  });
-  let content = JSON.parse(res.text);
-  if (content.code === 100000) {
-    return content.text;
-  } else {
-    return '出错了：' + content.text;
-  }
-}
-
-// 天行聊天机器人
-async function getReply(word) {
-  let url = TXHOST + 'robot/';
-  let res = await superagent.req(url, 'GET', {
-    key: config.TXAPIKEY,
-    question: word,
-    mode: 1,
-    datatype: 0,
-    userid: uniqueId
-  });
-  let content = JSON.parse(res.text);
-  if (content.code === 200) {
-    let response = '';
-    if (content.datatype === 'text') {
-      response = content.newslist[0].reply
-    } else if (content.datatype === 'view') {
-      response =`虽然我不太懂你说的是什么，但是感觉很高级的样子，因此我也查找了类似的文章去学习，你觉得有用吗<br>《${content.newslist[0].title}》${content.newslist[0].url}`
-    } else {
-      response = '你太厉害了，说的话把我难倒了，我要去学习了，不然没法回答你的问题';
+async function history(word){
+  if (word.includes('历史的今天') || word.includes('历史上的今天') || word.includes('历史今天') ) {
+    try {
+      let res = await superagent.req(
+        'https://api.asilu.com/today/'
+      );
+      let obj = JSON.parse(res.text);
+      if(obj.code === 200 && obj.data.lenth !== 0){
+        return obj.data.map((k) => k.year + '年:\n' + k.title + '\n' + k.link)
+        .join('\n');
+      }
+    } catch {
     }
-    return response;
-  } else {
-    return '我好像迷失在无边的网络中了，你能找回我么';
   }
 }
 
-async function getSweetWord() {
-  // 获取土味情话
-  let url = TXHOST + 'saylove/';
-  try {
-    let res = await superagent.req(url, 'GET', { key: config.TXAPIKEY });
-    let content = JSON.parse(res.text);
-    if (content.code === 200) {
-      let sweet = content.newslist[0].content;
-      let str = sweet.replace('\r\n', '<br>');
-      return str;
-    } else {
-      console.log('获取接口失败', content.msg);
+
+// 解梦
+async function jiemeng(word){
+  if (word.includes('解梦') || word.includes('周公解梦')) {
+    try {
+      let res = await superagent.req(
+        'http://v.juhe.cn/dream/query?cid=&full=&key=4214b27c051a4f1ac7f2551233790a19&q='+encodeURIComponent(word.replace(/解梦|周公解梦/g,''))
+      );
+      let obj = JSON.parse(res.text);
+      if(obj.reason === 'successed' && obj.result !== null){
+        console.log(obj.result)
+        return obj.result.map((k) => k.title + ':\n' + k.des + '\n')
+        .join('\n');
+      }else{
+        return await getQingYuke(word.replace('解梦',''))
+      }
+    } catch {
     }
-  } catch (err) {
-    console.log('获取接口失败', err);
   }
 }
 
-/**
- * 获取垃圾分类结果
- * @param {String} word 垃圾名称
- */
-
-async function getRubbishType(word) {
-  let url = TXHOST + 'lajifenlei/';
-  let res = await superagent.req(url, 'GET', { key: config.TXAPIKEY, word: word });
-  let content = JSON.parse(res.text);
-  if (content.code === 200) {
-    let type;
-    if (content.newslist[0].type == 0) {
-      type = '是可回收垃圾';
-    } else if (content.newslist[0].type == 1) {
-      type = '是有害垃圾';
-    } else if (content.newslist[0].type == 2) {
-      type = '是厨余(湿)垃圾';
-    } else if (content.newslist[0].type == 3) {
-      type = '是其他(干)垃圾';
-    }
-    let response =
-      content.newslist[0].name +
-      type +
-      '<br>解释：' +
-      content.newslist[0].explain +
-      '<br>主要包括：' +
-      content.newslist[0].contain +
-      '<br>投放提示：' +
-      content.newslist[0].tip;
-    return response;
-  } else {
-    console.log('查询失败提示：', content.msg);
-    return '暂时还没找到这个分类信息呢';
-  }
+async function a() {
+  console.log(await getONE('历史上的今天 黄斯蒂芬是的冯绍峰金'));
 }
+a();
 
 module.exports = {
-  getOne,
-  getTXweather,
-  getReply,
-  getSweetWord,
-  getTuLingReply,
-  getTXTLReply,
-  getRubbishType
+  getONE,
 };
